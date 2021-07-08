436a27e486b8f62f32a4a7f9a7899321addcc471
Dwire <gjdwyer8@gmail.com>
2021-07-08 17:25:16 -0400

    class work

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,15 @@
 class Magazine < ActiveRecord::Base
-  
+  has_many :subscriptions
+  has_many :readers, through: :subscriptions
+
+
+  def email_list
+    email_array = self.readers.map {|reader| reader.email}
+    email_array.join("; ")
+  end 
+
+
+  def self.most_popular
+    self.all.max_by {|mag| mag.subscriptions.count}
+  end 
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,21 @@
 class Reader < ActiveRecord::Base
+  has_many :subscriptions
+  has_many :magazines, through: :subscriptions
+
+
+  def subscribe(magazine, price)
+    Subscription.create(magazine_id: magazine.id, price: price, reader_id: self.id)
+  end 
+
+  def total_subcription_price
+    prices_array = self.subscriptions.map {|sub| sub.price}
+    prices_array.sum
+  end 
+
+  def cancel_subscription(magazine)
+    sub_instance = self.subscriptions.find_by(magazine_id: magazine.id)
+    sub_instance.destroy
+  end 
+
   
 end
